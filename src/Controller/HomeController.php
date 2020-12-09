eb386971013df38ce6c587edc18be6c52b78c7e8
CeliaBoiss <boisseau.celia@gmail.com>
2020-12-09 16:44:40 +0100

    make the coord and contact sections, try to send mail automaticaly when form send

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,4 +15,26 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig');
     }
+
+    /**
+     * @Route("/contact_mail", name="home_contact_mail")
+     */
+    public function sendMail(): Response
+    {
+        $from = $_POST['mail'];
+        $receiver = "wewelcome.test@gmail.com";
+        $subject = "Demande de contact : " . $_POST['firstName'] . " " . $_POST['lastName'];
+        $message = "Nom : " . $_POST['lastName'] . " Prénom : " . $_POST['firstName'] . " Sujet : "
+            . $_POST['subject'] . " Téléphone : " . $_POST['phone'] . " Mail : " . $_POST['mail'] .
+            " Message : " . $_POST['message'];
+
+        $success = mail($receiver, $subject, $message, "From: <$from>");
+
+        if ($success) {
+            var_dump("Votre message à bien été envoyé.");
+        } else {
+            var_dump("Une erreur s'est produite à l'envoi de votre message.");
+        }
+        return $this->render('home/index.html.twig');
+    }
 }
