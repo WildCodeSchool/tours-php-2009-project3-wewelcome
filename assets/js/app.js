c21c6a110a5d3683e59a6a9b725ba9fcc2bdf47a
thocle86 <thocle86@gmail.com>
2020-12-10 16:41:13 +0100

    Resolution problem html + scss files passed in css + removal of unused fonts

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,8 +6,8 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import '../styles/app.scss';
-import '../styles/_purpose_and_values.scss';
+import '../styles/app.css';
+import '../styles/_purpose_and_values.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
