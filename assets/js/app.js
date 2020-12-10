c552978d86303872be0be889e58941053fcedccb
thocle86 <thocle86@gmail.com>
2020-12-10 09:54:08 +0100

    Resolution of the following error, forgetting a semicolon

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../styles/app.scss';
-import '../styles/_purpose_and_values.scss'
+import '../styles/_purpose_and_values.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
