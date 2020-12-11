0ee79ee19b51b1556b6107e7c593b8558bc62d86
thocle86 <thocle86@gmail.com>
2020-12-11 14:10:41 +0100

    Add home section with carousel + code reorganization

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,6 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../styles/app.css';
-import '../styles/_purpose_and_values.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
