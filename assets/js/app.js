1d404ee841e3d28ae4e3c5747b06dc5153cfa745
thocle86 <thocle86@gmail.com>
2020-12-16 11:11:31 +0100

    update branch before pushing

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,8 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import '../styles/app.css';
+import '../styles/base.css';
+import '../styles/index.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
