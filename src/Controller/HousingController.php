8d0ab0f7230a326c78655b625e7fb9ceed9f09c2
Quentin-BLANC <quentinblanc587@gmail.com>
2021-01-06 17:20:33 +0100

    creating the housing page

---
+++ src/Controller/HousingController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\FormData\MailMessageData;
+use App\Form\MailMessageType;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+
+class HousingController extends AbstractController
+{
+    /**
+     * @Route("/housing", name="housing")
+     */
+    public function index(Request $request, MailerInterface $mailer): Response
+    {
+        $mailMessage = new MailMessageData();
+        $form = $this->createForm(MailMessageType::class, $mailMessage);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $email = (new TemplatedEmail())
+            ->from($mailMessage->getEmail())
+            ->to('wewelcome.test@gmail.com')
+            ->subject('Message client : ' . $mailMessage->getSubject())
+            ->htmlTemplate('emails/contact.html.twig')
+            ->context(['message' => $mailMessage]);
+
+            $mailer->send($email);
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('housing/index.html.twig', ['form' => $form->createView()]);
+    }
+}
