ca98c3b193dbec02ea01504f1bfa7b53c68b363a
CeliaBoiss <boisseau.celia@gmail.com>
2021-01-10 15:01:20 +0100

    admin can now login and log out the website

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -31,5 +31,6 @@ class SecurityController extends AbstractController
      */
     public function logout(): void
     {
+        /*'This method can be blank - it will be intercepted by the logout key on your firewall.'*/
     }
 }
